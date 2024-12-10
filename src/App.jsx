e6f7cd29f5e76cbf01f59e1ebfbc606c69dbd63a
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-10 12:50:02 -0300

    Refactor with a single example

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import sunsetIcon from "./assets/sunset.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
+      <img src={sunsetIcon} alt="Sunset Icon" className="w-40 h-40 mb-4" />
+      <h1 className="text-2xl font-bold text-blue-500">
+        ¡Hola, Sunset Experience!
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
