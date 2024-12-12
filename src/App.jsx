fa707a39c9ef891c392353e8c20d7b83ae215cdd
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-12 15:15:44 -0300

    Import the navigation bar and adjust the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
+  const navLinks = [
+    { label: "Home", href: "/" },
+    { label: "About", href: "/about" },
+    { label: "Blog", href: "/blog" },
+    { label: "Contact", href: "/contact" },
+  ];
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <NavigationBar links={navLinks} />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/blog" element={<div>Blog Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
+      </Routes>
+    </>
   );
 }
 
