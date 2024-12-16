74f70b916efbddbe9c0ec541bf5391c4f305b0cf
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-16 20:51:32 -0300

    Remove the banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import Banner from '../components/Banner';
+
 import Cards from '../components/Cards';
 
 const Home = () => {
   return (
     <div> 
-      <Banner />
       <Cards />
     </div>
   );
