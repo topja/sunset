c1bc18620ef62bf32d346df19473a47679b520cf
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-16 08:44:05 -0300

    Create the new Crads component to show the Tabs and the CardList components

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { tabs, cards } from "../data/bannerData";
+import Tabs from "./Tabs";
+import CardList from "./CardList";
+
+const Cards = () => {
+  const [activeTab, setActiveTab] = useState("kits");
+
+  return (
+    <div className="">
+      {/* Tabs */}
+      <Tabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      {/* Tarjetas */}
+      <div className="bg-white px-6 md:px-8 lg:px-10">
+        <CardList items={cards[activeTab]} />
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
