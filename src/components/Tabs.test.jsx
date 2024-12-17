c7885852f3c83ff5f628103d6562cffb18c410af
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-17 10:42:49 -0300

    Update the tabs tests

--- src/components/Tabs.test.jsx
+++ src/components/Tabs.test.jsx
@@ -20,7 +20,7 @@ describe("Tabs Component", () => {
     render(<Tabs tabs={mockTabs} activeTab="experiences" setActiveTab={() => {}} />);
 
     const activeTab = screen.getByText("Experiencias");
-    expect(activeTab).toHaveClass("bg-verdeOscuro text-white");
+    expect(activeTab).toHaveClass("border-verdeOscuro text-verdeOscuro");
   });
 
   it("calls setActiveTab when a tab is clicked", () => {
