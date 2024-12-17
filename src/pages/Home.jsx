40864550675729f81e6b95c8266603d55f4c8028
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-17 17:39:40 -0300

    Add the banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-
 import Cards from '../components/Cards';
+import Banner from '../components/Banner';
 
 const Home = () => {
   return (
     <div> 
+      <Banner />
       <Cards />
     </div>
   );
