b3887ba66bd2c7cbe38ec48acc5749f029b7ac52
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-17 17:40:31 -0300

    Update some styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,18 +3,18 @@ const Banner = () => {
 
   return (
     <div
-      className="relative h-[400px] md:h-[500px] lg:h-[600px] w-full bg-cover bg-center loading-lazy"
+      className="relative h-[400px] md:h-[500px] w-full bg-cover bg-center loading-lazy"
       style={{ backgroundImage: "url('/src/assets/ElTabo5.jpg')" }}
     >
       <div className="absolute inset-0 bg-black/20"></div>
     
 
       {/* Contenido Central */}
-      <div className="relative flex flex-col justify-center items-start h-full text-left px-4 md:px-12 lg:px-24 gap-2">
-        <h2 className="text-white text-2xl md:text-4xl lg:text-5xl font-bold">
+      <div className="relative flex flex-col justify-center items-center h-full text-left px-4 md:px-12 lg:px-24 gap-2">
+        <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-bold font-agbalumo mb-3">
           Limpia tu cuerpo, sana tu alma
-        </h2>
-        <p className="text-white font-bold text-base md:text-lg lg:text-xl">
+        </h1>
+        <p className="text-white font-inter font-bold text-base md:text-lg lg:text-xl">
           Empieza tu desconexión desde el primer momento.
         </p>
       </div>
