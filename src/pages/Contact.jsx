c6567f15fc0e621cc9497bae948fd13bcec10032
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-17 17:40:13 -0300

    Add a banner with title

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -50,150 +50,161 @@ const Contact = () => {
   };
 
   return (
-    <div className="mt-10 mb-10 px-4 lg:px-24 lg:mt-24 lg:mb-24">
-      <ToastContainer position="top-right" autoClose={3000} /> {/* Contenedor de Toastify */}
-      <div className="bg-gradient-to-r h-20 from-verdeOscuro to-gray-800 text-white my-6 rounded-t-xl">
-      </div> 
-
-      {/* Formulario */}
-      <div className="">
-        
-
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className="text-center shadow-lg py-10 px-2 border-2 border-gray-200 rounded-md">
-            <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-              Contáctanos
-            </h2>
-            <p className="mt-2 text-lg text-gray-600">
-              ¿Tienes consultas sobre nuestras experiencias o kits? Envíanos tu mensaje.
-            </p>
-            <div className="flex justify-center gap-10 mt-10">
-              <div className="flex flex-col items-center gap-2">
-                <img src={MailIcon} alt="Mail" className="h-10 w-10 p-2 rounded-full border-gray-400 border" />
-                <p className="font-bold">Correo</p>
-                <p>info@example.com</p>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <img src={PhoneIcon} alt="Phone" className="h-10 w-10 p-2 rounded-full border-gray-400 border" />
-                <p className="font-bold">Teléfono</p>
-                <p>+56 123 456 789</p>
-              </div>
-            </div>
-          </div>
-          {/* Formulario */}
-          <div className="bg-gray-800 text-white p-6 rounded-md shadow-md">
-            <form onSubmit={handleSubmit}>
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <div>
-                  <label htmlFor="firstName" className="block text-sm font-medium">
-                    Nombres
-                  </label>
-                  <input
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                    placeholder="Nombre completo"
-                    required
-                  />
-                </div>
-                <div>
-                  <label htmlFor="lastName" className="block text-sm font-medium">
-                    Apellidos
-                  </label>
-                  <input
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                    placeholder="Apellidos"
-                    required
-                  />
-                </div>
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium">
-                    Correo
-                  </label>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                    placeholder="Correo"
-                    required
-                  />
-                </div>
-                <div>
-                  <label htmlFor="number" className="block text-sm font-medium">
-                    Número telefónico
-                  </label>
-                  <input
-                    id="number"
-                    name="number"
-                    type="text"
-                    value={formData.number}
-                    onChange={handleChange}
-                    className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                    placeholder="Teléfono"
-                  />
-                </div>
-              </div>
-              <div className="mt-4">
-                <label htmlFor="message" className="block text-sm font-medium">
-                  Deja tu mensaje
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows="4"
-                  value={formData.message}
-                  onChange={handleChange}
-                  className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                  placeholder="Escribe tu mensaje aquí"
-                  required
-                />
-              </div>
-
-              {/* Botón */}
-              <button
-                type="submit"
-                className="mt-6 w-full sm:w-auto px-6 py-2 rounded-md bg-verdeOscuro text-white font-semibold hover:bg-indigo-500 transition-all"
-              >
-                {formStatus === "loading" ? "Enviando..." : "Enviar Mensaje"}
-              </button>
-            </form>
-          </div>
+    <>
+      <div className="relative w-full h-[400px] md:h-[500px] bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: "url('/src/assets/Piscina.avif')" }}>
+        <div className="absolute inset-0 bg-black/50"></div>
+        <div className="text-center relative z-10 text-white px-4">
+          <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-bold font-agbalumo mb-3">Contact Us</h1>
+          <p className="text-white font-inter font-bold text-base md:text-lg lg:text-xl">
+            Discover who we are and what drives us to provide exceptional experiences.
+          </p>
         </div>
       </div>
+      <ToastContainer position="top-right" autoClose={3000} /> {/* Contenedor de Toastify */}
+      <div className="mt-10 mb-10 px-4 lg:px-24 lg:mt-24 lg:mb-24">
+        <div className="bg-gradient-to-r h-20 from-white to-gray-800 text-white my-6 rounded-t-xl">
+        </div> 
 
-      {/* Mapa */}
-      <div className="w-full">
-          <p className="mb-2 mt-8 text-lg text-center">
-            Reserva con nosotros en{" "}
-            <a
-              href="https://www.airbnb.cl/rooms/753469374767232802?_set_bev_on_new_domain=1734363698_EANTUwYzM2ZDQ3MG&source_impression_id=p3_1734389820_P35FlgGnExNXUxD2" 
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-verdeOscuro font-bold hover:underline"
-            >
-              Airbnb
-            </a>
-          </p>
-          <iframe
-            title="Ubicación"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26081.5143879394!2d-71.6683081236221!3d-33.457312586493494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662160ac9bd0e47%3A0x147352da12254861!2sPlaza%20El%20Tabo!5e0!3m2!1ses!2scl!4v1734368862180!5m2!1ses!2scl"
-            className="w-full h-[400px] border-0 rounded-md shadow-md"
-            loading="lazy"
-            allowFullScreen
-          ></iframe>
+        {/* Formulario */}
+        <div className="">
+          
+
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <div className="text-center shadow-lg py-10 px-2 border-2 border-gray-200 rounded-md">
+              <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl font-agbalumo">
+                Contáctanos
+              </h2>
+              <p className="mt-2 text-lg text-gray-600">
+                ¿Tienes consultas sobre nuestras experiencias o kits? Envíanos tu mensaje.
+              </p>
+              <div className="flex justify-center gap-10 mt-10">
+                <div className="flex flex-col items-center gap-2">
+                  <img src={MailIcon} alt="Mail" className="h-10 w-10 p-2 rounded-full border-gray-400 border" />
+                  <p className="font-bold">Correo</p>
+                  <p>info@example.com</p>
+                </div>
+                <div className="flex flex-col items-center gap-2">
+                  <img src={PhoneIcon} alt="Phone" className="h-10 w-10 p-2 rounded-full border-gray-400 border" />
+                  <p className="font-bold">Teléfono</p>
+                  <p>+56 123 456 789</p>
+                </div>
+              </div>
+            </div>
+            {/* Formulario */}
+            <div className="bg-gray-800 text-white p-6 rounded-md shadow-md">
+              <form onSubmit={handleSubmit}>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                  <div>
+                    <label htmlFor="firstName" className="block text-sm font-medium">
+                      Nombres
+                    </label>
+                    <input
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                      placeholder="Nombre completo"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="lastName" className="block text-sm font-medium">
+                      Apellidos
+                    </label>
+                    <input
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                      placeholder="Apellidos"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email" className="block text-sm font-medium">
+                      Correo
+                    </label>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                      placeholder="Correo"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="number" className="block text-sm font-medium">
+                      Número telefónico
+                    </label>
+                    <input
+                      id="number"
+                      name="number"
+                      type="text"
+                      value={formData.number}
+                      onChange={handleChange}
+                      className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                      placeholder="Teléfono"
+                    />
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <label htmlFor="message" className="block text-sm font-medium">
+                    Deja tu mensaje
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows="4"
+                    value={formData.message}
+                    onChange={handleChange}
+                    className="mt-1 block w-full rounded-md bg-gray-700 p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Escribe tu mensaje aquí"
+                    required
+                  />
+                </div>
+
+                {/* Botón */}
+                <button
+                  type="submit"
+                  className="mt-6 w-full sm:w-auto px-6 py-2 rounded-md bg-verdeOscuro text-white font-semibold hover:bg-indigo-500 transition-all"
+                >
+                  {formStatus === "loading" ? "Enviando..." : "Enviar Mensaje"}
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        {/* Mapa */}
+        <div className="w-full">
+            <p className="mb-2 mt-8 text-lg text-center">
+              Reserva con nosotros en{" "}
+              <a
+                href="https://www.airbnb.cl/rooms/753469374767232802?_set_bev_on_new_domain=1734363698_EANTUwYzM2ZDQ3MG&source_impression_id=p3_1734389820_P35FlgGnExNXUxD2" 
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-verdeOscuro font-bold hover:underline"
+              >
+                Airbnb
+              </a>
+            </p>
+            <iframe
+              title="Ubicación"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26081.5143879394!2d-71.6683081236221!3d-33.457312586493494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662160ac9bd0e47%3A0x147352da12254861!2sPlaza%20El%20Tabo!5e0!3m2!1ses!2scl!4v1734368862180!5m2!1ses!2scl"
+              className="w-full h-[400px] border-0 rounded-md shadow-md"
+              loading="lazy"
+              allowFullScreen
+            ></iframe>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
