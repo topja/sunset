9deb293a08630d949f4ac7ccf902274ef437d65c
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-12-16 21:49:30 -0300

    Update the airbnb link

--- src/pages/Contact.test.jsx
+++ src/pages/Contact.test.jsx
@@ -21,7 +21,7 @@ describe("Contact Component", () => {
     expect(screen.getByRole("button", { name: /Enviar mensaje/i })).toBeInTheDocument();
 
     const airbnbLink = screen.getByRole("link", { name: /Airbnb/i });
-    expect(airbnbLink).toHaveAttribute("href", "https://airbnb.com");
+    expect(airbnbLink).toHaveAttribute("href", "https://www.airbnb.cl/rooms/753469374767232802?_set_bev_on_new_domain=1734363698_EANTUwYzM2ZDQ3MG&source_impression_id=p3_1734389820_P35FlgGnExNXUxD2");
 
     const googleMap = screen.getByTitle(/Ubicación/i);
     expect(googleMap).toBeInTheDocument();
