ae9c1fa2b82fb50ffa25526d81e0867de0ddc3bf
Ricardo Martínez <ricardomartinezvet@gmail.com>
2025-01-13 15:38:48 -0300

    Eliminar lorores en desuso

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        customBlue: "#008EC4",
-        azulSueve: "#EBF9FF",
-        verdeOscuro: "#008395",
-        fondo: "#F7F7F7",
         Charcoal: "#122620",
         Gold: "#D6AD60",
         Tan: "#B68D40",
