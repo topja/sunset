b4c37f522312adb1dcec93e47d166c92e30baaae
Ricardo Martínez <ricardomartinezvet@gmail.com>
2025-01-14 10:22:56 -0300

    Mejoramos manejo de error en servicio de contacto

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,23 +1,26 @@
 import emailjs from "@emailjs/browser";
 
-const sendContactEmail = async (formData) => {
-  const serviceId = "service_46sy2if";
-  const templateId = "template_pqssfqq";
-  const userId = "RZlA9W7LydyMmrvvO";
+export const sendContactEmail = async (formData) => {
+  const serviceId = "service_46sy2if"; 
+  const templateId = "template_pqssfqq"; 
+  const userId = "RZlA9W7LydyMmrvvO"; 
 
   try {
-    const response = await emailjs.send(serviceId, templateId, {
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      email: formData.email,
-      number: formData.number,
-      message: formData.message,
-    }, userId);
+    await emailjs.send(
+      serviceId,
+      templateId,
+      {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        number: formData.number,
+        message: formData.message,
+      },
+      userId
+    );
 
-    return { success: true, response };
+    return { success: true };
   } catch (error) {
-    return { success: false, error };
+    throw new Error(`Error al enviar el correo: ${error.message}`);
   }
 };
-
-export default sendContactEmail;
