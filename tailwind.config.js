d331590d2d23024771568786575bd07433b1a568
Ricardo Martínez <ricardomartinezvet@gmail.com>
2025-01-22 16:50:11 -0300

    Add airbnb colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ export default {
         Gold: "#D6AD60",
         Tan: "#B68D40",
         Cream: "#F4EBD0",
+        Air: "#ff385c",
+        AirHover: "#e61e4d",
       },
     },
       fontFamily: {
